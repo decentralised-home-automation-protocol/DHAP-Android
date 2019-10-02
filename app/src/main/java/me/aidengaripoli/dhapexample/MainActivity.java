7bd2f4d30f1c67aead857474ae0799288a921361
Daniel Milner <danielmmilner@gmail.com>
2019-10-02 22:33:57 +1000

    Prevented joining activity crash and added feedback to refresh list

--- app/src/main/java/me/aidengaripoli/dhapexample/MainActivity.java
+++ app/src/main/java/me/aidengaripoli/dhapexample/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
@@ -188,6 +189,10 @@ public class MainActivity extends AppCompatActivity implements
         dhap.refreshCensusList(censusList, new RefreshCensuslistCallbacks() {
             @Override
             public void censusListRefreshed() {
+                runOnUiThread(() -> {
+                    Toast.makeText(getApplicationContext(),"Census List refreshed",Toast.LENGTH_SHORT).show();
+                });
+
                 if (censusList.isEmpty()) {
                     displayNoDevicesFound();
                 } else {
